021e79ff48230ad5665da8ec02f76b041f5da060
atimine <adhamxudayberganovdev@gmail.com>
2024-01-10 17:37:23 +0500

    Get file

---
+++ getFile.js
@@ -0,0 +1,9 @@
+function getFile() {
+    let getFile = document.getElementById("file").value;
+
+    if (getFile) {
+        console.log(getFile);
+    } else {
+        alert("File did not come");
+    }
+}
